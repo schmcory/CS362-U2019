569fd2666d24c400d530ea926823f2e025129efe
schmcory <36436049+schmcory@users.noreply.github.com>
2019-07-17 22:02:29 -0500

    Update unittest1.c

--- projects/schmcory/dominion/unittest1.c
+++ projects/schmcory/dominion/unittest1.c
@@ -1,1 +1,12 @@
-unittest1.c
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include <stdio.h>
+
+/* BARON CARD UNIT TEST:
+* Player has two choices
+    *If the player has an estate card
+      *gain a new estate card
+      *discard an estate card (boolean) and win 4 coins
+*/
+
