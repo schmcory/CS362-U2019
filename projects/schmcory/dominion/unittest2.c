aca5172d530656cc5adeddadd0d5b294d8274269
schmcory <36436049+schmcory@users.noreply.github.com>
2019-07-21 22:07:14 -0500

    Update unittest2.c

--- projects/schmcory/dominion/unittest2.c
+++ projects/schmcory/dominion/unittest2.c
@@ -1,1 +1,15 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include "rngs.h"
 
+
+
+
+/* MINION CARD UNIT TEST:
+* Player has two choices
+      *Win 2 coins
+      *discard Minion card + 4 other cards
+          *players with 5+ cards discard hand, draw another 4 cards
+*/
