ee51c9017c9f18007e501b5de694e7b15517f973
schmcory <36436049+schmcory@users.noreply.github.com>
2019-08-04 18:12:20 -0500

    Update unittest1.c

--- projects/schmcory/andatranDominion/unittest1.c
+++ projects/schmcory/andatranDominion/unittest1.c
@@ -4,73 +4,195 @@
 #include <stdio.h>
 #include <string.h>
 
+//assert true function
+void asserttrue(int actual, int expected) {
+
+	if (actual, expected) {
+
+		printf("Test Passed!\n");
+	}
+
+	else {
+
+		printf("Test Failed!\n");
+	}
+}
+
 
 /* BARON CARD UNIT TEST:
-* Player has two choices
+*Buys increased by 1
+*Player has two choices
     *If the player has an estate card
-      *gain a new estate card
-      *discard an estate card (boolean) and win 4 coins
+      *gain a new estate card (choice1 = 0)
+      *discard an estate card (boolean) and win 4 coins (choice1 = 1)
 */
-
-
-int main () {   
-   //initialize variables from cardEffect function
-   int choice1 = 0;
+int main() {
+   int choice1;
    int choice2 = 0;
-   int choice3 = 0
-   int handPos = 0;
-   int bonus = 0; 
+   int choice3 = 0;
+   int handpos = 0;
+   int bonus = 0;
    
-   int randomSeed = 1000;
+	int randomSeed = 1000;
    
-   //code pulled from playdom.c
-   int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse,
-           sea_hag, tribute, smithy};
+   struct gameState prevState;
    struct gameState state;
-   initializeGame(2, k, randomSeed, &state); 
    
-   //initialize player 1's first card to baron
-   state.card[0][0] = baron; 
+   int currentPlayer = whoseTurn(&state); 
+   
+	int k[10] = { baron, ambassador, village, minion, mine, cutpurse,
+			sea_hag, tribute, smithy, council_room };
 
-   //code borrowed from player.c
-   memset(&state, 0, sizeof(struct gameState));
-   
-   printf("Unit Tests: Baron");
-   
-   /* UNIT TEST 1: Were the number of buys increased by 1" */
-   printf("Test Case 1: Testing whether the number of buys increased by 1");
-   
-   //function call to cardEffect  
-   cardEffect(baron, choice1, choice2, choice3, &state, handPos, &bonus);
-   
-   if(state.numBuys == 1) {
-         printf("Test passed.\n");
-   }
-   
-   else {
-      printf("Test failed.\n");
-   }
-   
-   /*UNIT TEST 2: Was an estate card discarded? */
-   printf("Test Case 2: Testing whether an estate card was discarded");
-   if(state.card_not_discarded == 1) {
-         printf("Test passed.\n");
-   }
-   
-   else {
-      printf("Test failed.\n");
-   }
-   
-   /* UNIT TEST 3: Are 4 coins earned when player discards estate card? */
-   printf("Test Case 3: Testing whether 4 coins have been earned after discarding an estate");
-   if(state.coins == 4) {
-      printf("Test passed.\n");  
-   }
-   
-   else {
-      printf("Test failed.\n");
-   }
-  
+   //counters
+	int prevBuys;
+   int prevEstateCards;	
+	int postEstateCards;	
+	int prevDiscardCount;
+	int postDiscardCount;
+	int prevCoins; 
 
-  return 0;      
+	// initialize a game state and player cards
+	initializeGame(2, k, randomSeed, &state);
+   
+   int i;  //incrementor
+   
+/*---------------------- UNIT TEST 1: Number of buys increased by 1 -----------------------------*/
+   printf("Test Case 1: Testing whether the number of buys increased by 1\n");
+
+	//copy the game state
+	memcpy(&prevState, &state, sizeof(struct gameState));
+
+   //for loop allows the test case to initiate for both choices 0 and 1
+   for(i = 0; i < 2; i++) {
+	   choice1 = i;
+
+      printf("If the player chooses to gain a new estate...")
+	
+       //sets prevBuys variable equal to the number of buys before the card is played
+       prevBuys = prevState.numBuys;
+
+	    //call the cardEffect function with the baron card
+	    cardEffect(baron, choice1, choice2, choice3, &prevState, handpos, &bonus);
+
+	    //assert true statements to see if test passed
+       assertTrue(previousBuys, prevState.numBuys - 1);
+   }
+/*-------------------------------------------- END UNIT TEST 1 --------------------------------------------*/
+   
+/*------------------------------------ UNIT TEST 2: Gains an estate --------------------------------------*/
+	printf("TEST 4: Player chooses to gain an estate \n");
+
+	//copy the game state
+	memcpy(&prevState, &state, sizeof(struct gameState));
+   
+   //set choice1 = 0
+
+   //sets the prevEstateCards to 0
+   prevEstateCards = 0;
+   
+	//count the number of estates in the players hand before the card is played
+	for (i = 1; i < prevEstateCards.handCount[currentPlayer]; i++) {
+      //if the player is holding an estate card
+		//set the prevEstateCards variable equal to the estate cards the player is holding
+		if (prevState.hand[currentPlayer][i - 1] == estate) {
+			prevEstateCards += 1;
+		}
+	}
+
+   //sets the prevDiscardCount to 0 
+	prevDiscardCount = 0;
+   
+   //count the number of discarded estate cards from the players hand before the card is played
+	for (i = 0; i < prevState.discardCount[currentPlayer]; i++) {
+		if (prevState.deck[currentPlayer][i] == estate) {
+			prevDiscardCount += 1;
+		}
+	}
+
+	//call the cardEffect function
+	cardEffect(baron, choice1, choice2, choice3, &prevState, handpos, &bonus);
+
+	//count the number of estates in the players hand after the card is played
+	postEstateCards = 0;
+	for (i = 1; i < prevState.handCount[currentPlayer]; i++) {
+		if (prevState.hand[currentPlayer][i - 1] == estate) {
+			postEstateCards += 1;
+		}
+	}
+
+   //count the number of discarded estate cards from the players hand after the card is played
+	postDiscardCount = 0;
+	for (i = 0; i < prevState.discardCount[thisPlayer]; i++) {
+		if (prevState.deck[thisPlayer][i] == estate) {
+			postDiscardCount += 1;
+		}
+	}
+
+	//assert true statements to see if test passed
+	assertTrue(numberOfEstatesBefore, numberOfEstatesAfter - 1);
+	assertTrue(numberOfCoinsBefore, testG.coins);
+	assertTrue(numberOfEstatesBefore, numberOfEstatesAfter - 1);
+/*-------------------------------------------- END UNIT TEST 2 --------------------------------------------*/
+   
+/*---------------------- UNIT TEST 3: Estate card discarded and coins gained by 4 --------------------------------------*/
+   printf("Test Case 3: Testing whether an estate card was discarded and four coins were gained\n")
+
+	//copy the game state
+	memcpy(&prevState, &state, sizeof(struct gameState));
+   
+   //set choice1 = 1
+	choice1 = 1;
+
+   //sets the prevEstateCards to 0
+   prevEstateCards = 0;
+   
+	//count the number of estates in the players hand before the card is played
+	for (i = 1; i < prevEstateCards.handCount[currentPlayer]; i++) {
+      //if the player is holding an estate card
+		//set the prevEstateCards variable equal to the estate cards the player is holding
+		if (prevState.hand[currentPlayer][i - 1] == estate) {
+			prevEstateCards += 1;
+		}
+	}
+
+   //sets the prevDiscardCount to 0 
+	prevDiscardCount = 0;
+   
+   //count the number of discarded estate cards from the players hand before the card is played
+	for (i = 0; i < prevState.discardCount[currentPlayer]; i++) {
+		if (prevState.deck[currentPlayer][i] == estate) {
+			prevDiscardCount += 1;
+		}
+	}
+   
+   //set the prevCoins of to the state coins 
+	prevCoins = state.coins;
+   
+	//call the cardEffect function
+	cardEffect(baron, choice1, choice2, choice3, &prevState, handpos, &bonus);
+
+	//count the number of estates in the players hand after the card is played
+	postEstateCards = 0;
+	for (i = 1; i < prevState.handCount[currentPlayer]; i++) {
+		if (prevState.hand[currentPlayer][i - 1] == estate) {
+			postEstateCards += 1;
+		}
+	}
+
+   //count the number of discarded estate cards from the players hand after the card is played
+	postDiscardCount = 0;
+	for (i = 0; i < prevState.discardCount[thisPlayer]; i++) {
+		if (prevState.deck[thisPlayer][i] == estate) {
+			postDiscardCount += 1;
+		}
+	}
+   
+	//assert true statements to see if test passed
+	assertTrue(prevEstateCards, postEstateCards + 1);
+	assertTrue(prevState.coins, prevCoins + 4);
+	assertTrue(prevDiscardCount, postDiscardCount - 1);
+/*-------------------------------------------- END UNIT TEST 3 --------------------------------------------*/
+
+	return 0;
 }
+   
