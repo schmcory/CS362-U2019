25ffb56332993f1f499974ee902fcff2a6d35e3d
schmcory <36436049+schmcory@users.noreply.github.com>
2019-08-04 18:54:16 -0500

    Update unittest2.c

--- projects/schmcory/andatranDominion/unittest2.c
+++ projects/schmcory/andatranDominion/unittest2.c
@@ -4,70 +4,122 @@
 #include <stdio.h>
 #include "rngs.h"
 
+//assert true function
+void assertTrue(int actual, int expected) {
+
+	if (actual, expected) {
+
+		printf("Test Passed!\n");
+	}
+
+	else {
+
+		printf("Test Failed!\n");
+	}
+}
+
 
 /* MINION CARD UNIT TEST:
 * Player has two choices
-      *Win 2 coins
-      *discard Minion card + 4 other cards
+      *Win 2 coins (choice1) 
+      *discard Minion card + 4 other cards (choice2)
           *players with 5+ cards discard hand, draw another 4 cards
 */
 
 
 int main() {
+   int choice1;
+   int choice2 = 0;
+   int choice3 = 0;
+   int handpos = 0;
+   int bonus = 0;
+   
    int randomSeed = 1000;
    
-   //initialize variables from cardEffect function
-   int choice1 = 0;
-   int choice2 = 0;
-   int choice3 = 0
-   int handPos = 0;
-   int bonus = 0; 
-   
-   //code pulled from playdom.c
-   int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse,
-           sea_hag, tribute, smithy};
+   struct gameState prevState;
    struct gameState state;
-   initializeGame(2, k, randomSeed, &state);
-     
-   //initialize player's card to minion
-   state.card[0][0] = minion;
-     
-   //function call to cardEffect   
-   cardEffect(minion, choice1, choice2, choice3, &state, handPos, &bonus); 
    
-   //code borrowed from player.c
-   memset(&state, 0, sizeof(struct gameState)); 
-     
-   printf("Unit Tests: Minion");
-   /* UNIT TEST 1: Were the number of coins increased by 2?" */
-   printf("Test Case 1: Testing whether 2 coins were earned");
-   if(state.coins == 2) {
-       printf("Test passed.\n");
-   }
+   int currentPlayer = 0;
+   int nextPlayer = 1; 
    
-   else {
-      printf("Test failed.\n");
-   }
-     
-   /* UNIT TEST 2: Did the player discard the minion card?" */   
-   printf("Test Case 2: Testing whether the player discarded the minion card"); 
-      if(state.discardCount[0] == 1) {
-       printf("Test passed.\n");
-   }
-   
-   else {
-      printf("Test failed.\n");
-   }
+   int k[10] = { baron, ambassador, village, minion, mine, cutpurse,
+			sea_hag, tribute, smithy, council_room };
 
-   /* UNIT TEST 3: Did the player discard 4 other cards?" */ 
-   printf("Test Case 3: Testing whether player1 discarded 5 total cards"); 
-   if(state.discardCount == 5) {
-          printf("Test passed.\n");
+   //counters
+   int prevActions;	
+   int postActions;	
+   int prevCoins;
+   int prevHandCount;
+   int prevHandCount2;
+
+   // initialize a game state and player cards
+   initializeGame(2, k, randomSeed, &state);
+   
+   int i;  //incrementor
+   
+/*---------------------- UNIT TEST 1: Coins increased by 2 -----------------------------*/
+   printf("Test Case 1: Testing whether player won two coins\n");
+
+	//copy the game state
+	memcpy(&prevState, &state, sizeof(struct gameState));
+	
+     //sets the prevActions variable to the number of actions before the card is played
+     prevActions = prevState.numActions; 
+     
+     //sets the prevCoins variable to the number of coins before the card is played
+     prevCoins = prevState.coins;
+     
+     //sets prevHandCount variable equal to the handCount of the currentPlayer before the card is played 
+     prevHandCount = prevState.handCount[currentPlayer];
+     
+     choice1 = 1;
+     choice2 = 0;
+
+	//call the cardEffect function with the baron card
+	cardEffect(minion, choice1, choice2, choice3, &prevState, handpos, &bonus);
+
+	//assert true statements to see if test passed
+     asserttrue(prevState.numActions, prevActions);
+     asserttrue(prevState.coins, prevCoins + 2);
+     asserttrue(prevState.handCount[currentPlayer], prevHandCount - 1);
    }
+/*-------------------------------------------- END UNIT TEST 1 --------------------------------------------*/
    
-   else {
-      printf("Test failed.\n");
-   } 
+/*------------------------------------ UNIT TEST 2: Discards Minion card + 4 other cards, next player (with 5+ cards) draws another 4 cards --------------------------------------*/
+	printf("Test Case 2: Testing whether player discards minion card + 4 other cards\n");
+
+     //test whether player 2 has 5 cards
+     for(i = 0; i < 5; i++) {
+          drawCard(1, &state); 
+     }
+
+	//copy the game state
+	memcpy(&prevState, &state, sizeof(struct gameState));
    
-     return 0;  
+     //sets the prevActions variable to the number of actions before the card is played
+     prevActions = prevState.numActions; 
+     
+     //sets the prevCoins variable to the number of coins before the card is played
+     prevCoins = prevState.coins;
+     
+     //sets prevHandCount variable equal to the handCount of the currentPlayer before the card is played 
+     prevHandCount = prevState.handCount[currentPlayer];  
+
+     //sets the postHandCount variable equal to the handCount of the nextPlayer before the card is played
+     prevHandCount2 = prevState.handCount[nextPlayer];
+
+     choice1 = 0;
+     choice2 = 1;
+
+     //call the cardEffect function with the baron card
+	cardEffect(minion, choice1, choice2, choice3, &prevState, handpos, &bonus);
+   
+	//assert true statements to see is test passed
+     asserttrue(prevState.numActions, prevActions);
+     asserttrue(prevState.coins, prevCoins);
+     asserttrue(prevState.handCount, 4);
+     asserttrue(prevState.handCount, 4);
+/*-------------------------------------------- END UNIT TEST 2 --------------------------------------------*/
+
+	return 0;
 }
