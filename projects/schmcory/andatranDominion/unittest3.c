62cafae0f89cb5cbdccf3b5ce9c86974d31a0bdc
schmcory <36436049+schmcory@users.noreply.github.com>
2019-08-04 19:37:51 -0500

    Update unittest3.c

--- projects/schmcory/andatranDominion/unittest3.c
+++ projects/schmcory/andatranDominion/unittest3.c
@@ -5,6 +5,21 @@
 #include "rngs.h"
 
 
+//assert true function
+void assertTrue(int actual, int expected) {
+
+	if (actual, expected) {
+
+		printf("Test Passed!\n");
+	}
+
+	else {
+
+		printf("Test Failed!\n");
+	}
+}
+
+
 /* AMABASSADOR CARD UNIT TEST:
 * Player has one choice
     *The player can return 2 copies of any card from their hand back to supply
@@ -12,39 +27,80 @@
 */
 
 int main() {
-   int randomSeed = 1000;
-  
-   //initialize variables from cardEffect function
-   int choice1 = 0;
+   int choice1;
    int choice2 = 0;
-   int choice3 = 0
-   int handPos = 0;
-   int bonus = 0; 
+   int choice3 = 0;
+   int handpos = 0;
+   int bonus = 0;
    
-   //code pulled from playdom.c
-   int k[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse,
-           sea_hag, tribute, smithy};
+   int randomSeed = 1000;
+   
+   struct gameState prevState;
    struct gameState state;
-   initializeGame(2, k, randomSeed, &state); 
-  
-   state.card[0][0] = ambassador; 
+   
+   int currentPlayer = 0;
+   int nextPlayer = 1; 
+   
+   int k[10] = { baron, ambassador, village, minion, mine, cutpurse,
+			sea_hag, tribute, smithy, council_room };
 
-   //code pulled from player.c
-   memset(&state, 0, sizeof(struct gameState));
-  
-   cardEffect(ambassador, choice1, choice2, choice3, &state, handPos, &bonus); 
-  
-   printf("Unit Tests: Ambassador");
-  /* UNIT TEST 1: " */
-   printf("Test Case 1: Testing whether the player discarded the two cards"); 
-      if(state.discardCount[0] == 2) {
-       printf("Test passed.\n");
-   }
+   //counters
+   int prevHandCount1;	
+   int postHandCount1;
+   int prevHandCount2;	
+   int postHandCount2;
+
+   // initialize a game state and player cards
+   initializeGame(2, k, randomSeed, &state);
    
-   else {
-      printf("Test failed.\n");
-   }
+   int i;  //incrementor
    
-     return 0;  
+/*---------------------- UNIT TEST 1: Returns 2 cards -----------------------------*/
+   printf("Test Case 1: Testing whether player returned 2 cards\n");
+
+	//copy the game state
+	memcpy(&prevState, &state, sizeof(struct gameState));
+	
+     //sets the coins to 0
+     state.coins = 0; 
   
-}
+  prevHandCount1 = prevState.handCount[currentPlayer]; 
+  postHandCount1 = state.handCount[currentPlayer];
+  prevHandCount2 = prevState.handCount[nextPlayer]; 
+  postHandCount3 = state.handCount[nextPlayer];  
+  
+  //for loop sets hand of both players
+  for(int i = 0; i < 2; i++) {
+     for(int j = 0; j < 5; j++ {
+         if(j == 0) {
+               state.hand[i][j] = tribute;  
+         }
+        
+         else if(j == 5 - 1) {
+               state.hand[i][j] = ambassador;
+         }
+        
+         else {
+               state.hand[i][j] = gold;
+         }
+     }
+  }
+         
+  state.handCount[nextPlayer] = 5; 
+         
+  choice1 = 0;
+  choice2 = 2;
+  handPos = 4;
+  state.whoseTurn = currentPlayer;
+
+//call the cardEffect function with the ambassador card
+cardEffect(ambassador, choice1, choice2, choice3, &prevState, handpos, &bonus);
+
+	//assert true statements to see if test passed
+     asserttrue(postHandCount1, prevHandCount1);
+     asserttrue(postHandCount2, prevHandCount2);
+     asserttrue(state.discardCount[nextPlayer], prevState.discardCount[nextPlayer]);
+   }
+/*-------------------------------------------- END UNIT TEST 1 --------------------------------------------*/
+   return 0;  
+ }
